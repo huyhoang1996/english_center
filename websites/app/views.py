5dc5d497c7f7b1318b94d144b5723252ed8aef13
huyhoang1996ha <huyhoang1996ha@gmail.com>
2019-07-24 11:25:04 +0700

    Test get ip

--- websites/app/views.py
+++ websites/app/views.py
@@ -4,10 +4,19 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 def home(request):
     print "***START Power Card Introduction PAGE***"
     # try:
+    print 'ipppp ', get_client_ip(request)
+
     return render(request, 'websites/home.html')
     # except Exception, e:
     #     print "Error: ", e
